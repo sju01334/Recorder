f9b5e5d16f81334e8c39f078d43aff1de61519ba
sju01334 <sju01334@gmail.com>
2022-08-01 11:33:11 +0900

    권한부여

--- app/src/main/java/com/nepplus/recorder/MainActivity.kt
+++ app/src/main/java/com/nepplus/recorder/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.nepplus.recorder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,17 +10,45 @@ class MainActivity : AppCompatActivity() {
     private  val recordButton : RecordButton by lazy {
         findViewById(R.id.recordButton)
     }
-    
+
+    private  val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+
     private var state = State.BEFORE_RECORDING
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        requestAudioPermission()
         initView()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPemissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPemissionGranted){
+            finish()
+        }
+    }
+
+    private fun requestAudioPermission(){
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
     }
 
     private fun initView(){
         recordButton.updateIconWithState(state)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
